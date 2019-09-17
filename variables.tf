2576dc05a13c3286f5040bc43a3b904c8a4efc51
paulg <paulg@docker.lan>
2019-09-17 22:43:53 +0000

    first commit

---
+++ variables.tf
@@ -0,0 +1,26 @@
+variable "ecs-cluster-name" {
+  description = "The name for the cluster."
+  default     = "tf-ecs-cluster"
+}
+
+variable "ecs-key-pair-name" {
+  description = "The name for the cluster."
+  default     = "pgomersbach"
+}
+
+/*====
+environment specific variables
+======*/
+
+variable "production_database_name" {
+  description = "The database name for Production"
+}
+
+variable "production_database_username" {
+  description = "The username for the Production database"
+}
+
+variable "production_database_password" {
+  description = "The user password for the Production database"
+}
+
---
+++ vpc/internet-gateway.tf
@@ -0,0 +1,8 @@
+resource "aws_internet_gateway" "demo-vpc-internet-gateway" {
+  vpc_id = aws_vpc.demo-vpc.id
+
+  tags = {
+    Name = "demo-vpc-internet-gateway"
+  }
+}
+
