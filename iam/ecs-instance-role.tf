9c6fcb4cccba02cc9df9e8ce8016ddc3d830efd3
Paul Gomersbach <paul.gomersbach@mn.nl>
2019-10-01 07:13:57 +0000

    add policy

--- iam/ecs-instance-role.tf
+++ iam/ecs-instance-role.tf
@@ -4,7 +4,7 @@ resource "aws_iam_role" "ecs-instance-role" {
     assume_role_policy  = "${data.aws_iam_policy_document.ecs-instance-policy.json}"
 }
 
-data "aws_iam_policy_document" "ecs-instance-policy" {
+ data "aws_iam_policy_document" "ecs-instance-policy" {
     statement {
         actions = ["sts:AssumeRole"]
 
@@ -13,7 +13,57 @@ data "aws_iam_policy_document" "ecs-instance-policy" {
             identifiers = ["ec2.amazonaws.com"]
         }
     }
+} 
+
+/*
+data "aws_iam_policy_document" "ecs-instance-policy" {
+  statement {
+      actions = ["sts:AssumeRole"]
+
+      principals {
+          type        = "Service"
+          identifiers = ["ec2.amazonaws.com"]
+      }
+  }
+  statement {
+    sid       = ""
+    effect    = "Allow"
+    resources = ["*"]
+
+    actions = [
+      "ec2:AttachVolume",
+      "ec2:CreateVolume",
+      "ec2:CreateSnapshot",
+      "ec2:CreateTags",
+      "ec2:DeleteVolume",
+      "ec2:DeleteSnapshot",
+      "ec2:DescribeAvailabilityZones",
+      "ec2:DescribeInstances",
+      "ec2:DescribeVolumes",
+      "ec2:DescribeVolumeAttribute",
+      "ec2:DescribeVolumeStatus",
+      "ec2:DescribeSnapshots",
+      "ec2:CopySnapshot",
+      "ec2:DescribeSnapshotAttribute",
+      "ec2:DetachVolume",
+      "ec2:ModifySnapshotAttribute",
+      "ec2:ModifyVolumeAttribute",
+      "ec2:DescribeTags",
+      "elasticfilesystem:CreateFileSystem",
+      "elasticfilesystem:CreateMountTarget",
+      "ec2:DescribeSubnets",
+      "ec2:DescribeNetworkInterfaces",
+      "ec2:CreateNetworkInterface",
+      "elasticfilesystem:CreateTags",
+      "elasticfilesystem:DeleteFileSystem",
+      "elasticfilesystem:DeleteMountTarget",
+      "ec2:DeleteNetworkInterface",
+      "elasticfilesystem:DescribeFileSystems",
+      "elasticfilesystem:DescribeMountTargets",
+    ]
+  }
 }
+*/
 
 resource "aws_iam_role_policy_attachment" "ecs-instance-role-attachment" {
     role       = "${aws_iam_role.ecs-instance-role.name}"
