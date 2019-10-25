f8b2858238876bc0585cf68a3a95add6485af3df
Paul Gomersbach <paul.gomersbach@mn.nl>
2019-10-25 14:33:36 +0000

    zie todo

--- ec2/launch-configuration.tf
+++ ec2/launch-configuration.tf
@@ -107,7 +107,13 @@ resource "aws_security_group" "instance-security-group" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-
+  // apm-server
+  ingress {
+    from_port   = 8200
+    to_port     = 8200
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port   = 0
